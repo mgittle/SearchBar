45ebc878c43a59aea878937036498a4f10a1e730
Matthew Gittleman <gittleman.matthew@gmail.com>
2020-01-28 17:45:13 -0600

    basic navbar formatting

--- client/src/components/InputForm.jsx
+++ client/src/components/InputForm.jsx
@@ -8,6 +8,17 @@ const InputForm = ({ items }) => {
         <button type="submit">
           <i className="fa fa-search"></i>
         </button>
+        <br />
+        <div className="tags">
+          <div className="tagitem">Today's Deals</div>
+          <div className="tagitem">Best Seller's</div>
+          <div className="tagitem">Customer Service</div>
+          <div className="tagitem">Find a Gift</div>
+          <div className="tagitem">New Releases</div>
+          <div className="tagitem">Registry</div>
+          <div className="tagitem">Gift Cards</div>
+          <div className="tagitem">Sell</div>
+        </div>
       </form>
     </div>
   );
